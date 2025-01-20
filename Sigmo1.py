487420004919be95b4dce4cf6ce2936ef108922d
Ф. Алексей <alexeyfedotov272009@gmail.com>
2025-01-20 13:54:35 +0300

    omg idk what is it

--- Sigmo1.py
+++ Sigmo1.py
@@ -1,1 +1,11 @@
-print('hello world')
+# print('hello world')
+#
+# s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+# print(s[7])
+#
+# s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+# print(s[-10])
+#
+# a = input()
+# for i in range(0,len(a), 2):
+#     print(a[i])
