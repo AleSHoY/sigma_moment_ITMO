5f365739ab34f2600884c3e5c6012d8fcd136c85
Ф. Алексей <alexeyfedotov272009@gmail.com>
2025-01-27 13:58:25 +0300

    27/01/25

---
+++ chego.py
@@ -0,0 +1,36 @@
+# c = input()
+# print(c[::-1])
+# for i in range(1, len(c) + 1):
+#     print(c[-i])
+
+# a = input()
+# b = input()
+# c = input()
+# print(a[0])
+# print(b[0])
+# print(c[0])
+
+# a = input()
+# c = 0
+# for i in range(0, len(a)):
+#     c += int(a[i])
+# print(c)
+
+# a = input()
+# for i in range(len(a)):
+#     if a[i] in '1234567890':
+#         print('цифра')
+#         break
+# else:
+#     print('Цифр нет')
+#
+# a = input()
+# n = 0
+# m = 0
+# for i in range(len(a)):
+#     if a[i] in '+':
+#         n += 1
+#     elif a[i] in '*':
+#         m += 1
+# print(f'Символ + встречается {n} раз')
+# print(f'Символ * встречается {m} раз')
